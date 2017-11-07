991624ca215d1b408c73204f010cdf7ecd73d16b
MongoPark <33224920+MongoPark@users.noreply.github.com>
2017-11-07 10:22:42 +0200

    Update Grader.rb

--- Grader.rb
+++ Grader.rb
@@ -23,43 +23,4 @@ def evaluate_grade(score)
 
 
  
- #Solution 2
- puts "Kindly Enter your score"
-score=gets.chomp.to_i 
-   if score < 0 || score>100
-     puts "#{score} is an invalid SCORE,score has to be 0-100"
-   elsif (91..100) === score
-     puts " Score #{score} = Grade A"
-   elsif  (81..90) === score
-     puts " Score #{score} = Grade  B"
-     elsif  (71..80) === score
-     puts " Score #{score} = Grade  C"
-      elsif  (61..70) === score
-     puts " Score #{score} = Grade  D"
-     elsif  (51..60) === score
-     puts " Score #{score} = Grade  E"
-   else 
-     puts "YOU FAILED"
- end
-
-
-
  
- #Solution 3
-puts "Kindly Enter your score"
-score=gets.chomp.to_i 
-   if score < 0 || score>100
-     puts "Your #{score} is an invalid SCORE,score has to be 0-100"
-   elsif score>=91 && score <= 100
-     puts " Your Score #{score} = Grade A"
-   elsif  score>=81 && score <= 90
-     puts "Your Score #{score} = Grade  B"
-     elsif  score>=71 && score <= 80
-     puts "Your Score #{score} = Grade  C"
-      elsif score>=61 && score <= 70
-     puts " Your Score #{score} = Grade  D"
-     elsif  score>=51 && score <= 60
-     puts " Your Score #{score} = Grade  E"
-   else 
-     puts "YOU FAILED"
- end
